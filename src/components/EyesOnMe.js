c502bee3fee55709bf2f665257375346346a027a
Andrej <andrejtodoroski@yahoo.com>
2024-04-04 10:47:02 -0400

    finished lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+import react from "react"
+
+export default function EyesOnMe(){
+    return(
+        <div>
+            <button onFocus={(event) => console.log("Good!")} onBlur={(event) => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+        )
+}
